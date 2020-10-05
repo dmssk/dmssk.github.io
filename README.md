3f3ce0f2aaa647d6cd21c8a37221d910834435ee
Dmytro Puzyrenko <Dmytro.Puzyrenko@infopulse.com>
2020-10-05 22:54:03 +0300

    init

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# dmssk.github.io
-site
-14.02.19 - uploaded my practice web-site
+# imcompany
---
+++ src/scripts/index.js
@@ -0,0 +1,11 @@
+import '../styles/main.scss';
+import $ from 'jquery';
+
+
+$(window).on('load', function () {
+
+
+
+
+});
+
